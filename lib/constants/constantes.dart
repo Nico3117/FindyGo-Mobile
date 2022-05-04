992871a68abee5d5b91612dfca7670e085830c6c
Aurelien49 <92448486+AurelienV49@users.noreply.github.com>
2022-05-04 14:35:32 +0200

    Améioration traductions

--- lib/constants/constantes.dart
+++ lib/constants/constantes.dart
@@ -1,6 +1,6 @@
 class ConstApp{
   static const String appName = 'FindyGo';
   static const bool ADMIN_MODE = false;
-  static const String FINDYGO_IT_SUPPORT_EMAIL = "aurelien.vaillant49@ynov.com";
+  static const String FINDYGO_IT_SUPPORT_EMAIL = "findygo.assos@gmail.com";
 }
 
